d5e7d00d82a2df2678aafe3d423a2067c2be6313
root <lemonsoldout@163.com>
2022-07-17 02:56:47 +0100

    update main FastAPI

--- src/main.py
+++ src/main.py
@@ -343,4 +343,4 @@ class csvInfo(BaseModel):
     aircraft_num : int=None
     
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=3000)    
+    uvicorn.run(app, host="0.0.0.0", port=3000)    
