f60bd53a1d0070708f47de54c80909d33afd1c7e
lemonsoldout <lemonsoldout@163.com>
2022-07-22 01:19:22 -0400

    update FastAPI start requests

--- src/main.py
+++ src/main.py
@@ -24,6 +24,8 @@ from pydantic import BaseModel
 import uvicorn
 import streamlit_authenticator as stauth
 from sys import argv
+import boto3
+import time
 ################################################################
 
 
@@ -59,70 +61,192 @@ else:
     key = f.read()
     f.close()
 
-########################################################
+
+######################################################## create `fastapi.yaml` and `trained_model.h5` ########################################################
+"""
+@date 7/21/2022
+@author Cheng Wang
+@Description: Create fastapi account and download trained model from AWS S3 using AWS S3 credentials
+"""
+
 result={}
-if len(argv) == 1:
-    os.system("echo 'You need input your username, full_name, email, password!!!'")
+s3_credentials = {}
+try:
+    if os.path.exists("fastapi.yaml") and os.path.exists("trained_model.h5"):
+        print("\n\033[0;35;40m `fastapi.yaml` and  `trained_model.h5` file exists!\033[0m\n\n\n")
+        time.sleep(1)
+    
+    elif os.path.exists("fastapi.yaml") and not os.path.exists("trained_model.h5"):
+        # 5 params
+        if len(argv) == 6:
+            s3_credentials["service_name"] = argv[1]
+            s3_credentials["region_name"] = argv[2]
+            s3_credentials["aws_access_key_id"] = argv[3]
+            s3_credentials["aws_secret_access_key"] = argv[4]
+            s3_credentials["aws_s3_bucket_name"] = argv[5]
+
+            s3_resource = boto3.client(
+                service_name=s3_credentials['service_name'],
+                region_name=s3_credentials['region_name'],
+                aws_access_key_id=s3_credentials['aws_access_key_id'],
+                aws_secret_access_key=s3_credentials['aws_secret_access_key'])
+
+            bucket = s3_credentials['aws_s3_bucket_name']
+
+            key = 'model/trained_model.h5'
+
+            s3_resource.download_file(bucket,key,Filename="trained_model.h5")
+            if os.path.exists("trained_model.h5"):
+                print("\n\033[0;32;40mSuccessfully create `trained_model.h5` file!\033[0m\n")
+                # raise ValueError("\033[0;33;40mYou need input AWS S3 credentials: service_name, region_name, aws_access_key_id, aws_secret_access_key, aws_s3_bucket_name!\033[0m")
+            
+            
+            print("service name:\t\t\t\033[1;36;40m",s3_credentials["service_name"],"\033[0m")
+            print("region name:\t\t\t\033[1;36;40m",s3_credentials["region_name"],"\033[0m")
+            print("aws access key id:\t\t\033[1;36;40m",s3_credentials["aws_access_key_id"],"\033[0m")
+            print("aws secret access key:\t\t\033[1;36;40m",s3_credentials["aws_secret_access_key"],"\033[0m")
+            print("aws s3 bucket name:\t\t\033[1;36;40m",s3_credentials["aws_s3_bucket_name"],"\033[0m\n")
+        
+        else:
+            raise IOError("\033[0;33;40mWe have a strict check here in order to confirm you have correct AWS S3 credentials!\nFormat:\n1. service_name\n2. region_name\n3. aws_access_key_id\n4. aws_secret_access_key\n5. aws_s3_bucket_name\033[0m")
+            
+        
+    elif os.path.exists("trained_model.h5") and not os.path.exists("fastapi.yaml"):
+        # at least 2 params
+        # at most 4 params
+        # status = false 
+
+        if(len(argv) < 3):
+            print("\n\033[0;31;40mError: No fastapi.yaml file!\033[0m")
+            raise IOError("\033[0;33;40mAt least you need input your username and password!\033[0m")
+        
+        username = argv[1]
+        result[username] = {}
+        result[username]['username'] = argv[1]
+        list_passwd = []
+        list_passwd.append(argv[2])
+        list_hashed_res = stauth.Hasher(list_passwd).generate()
+        result[username]['hashed_password'] = list_hashed_res[0]
+
+        if len(argv) == 3:
+            result[username]['full_name'] = argv[1]
+            result[username]['email'] = ""
+
+        elif(len(argv) == 4):
+            result[username]['full_name'] = argv[3]
+            result[username]['email'] = ""
+           
+        elif(len(argv) == 5):
+            result[username]['full_name'] = argv[3]
+            result[username]['email'] = argv[4]
+        elif(len(argv > 5)):
+            print("\033[0;33;40mWe only accept at most 4 params here!\nFormat:\n1. username\n2. password\n3. full_name\n4. email\n\033[0m")
+            
+        result[username]['disabled'] = False
+        with open('fastapi.yaml', 'w', encoding='utf-8') as file:
+            yaml.dump(data=result, stream=file, allow_unicode=True)
+                
+        if os.path.exists("fastapi.yaml"):
+            print("\n\033[0;32;40mSuccessfully create `fastapi.yaml` file!\033[0m\n")
+        else:
+            raise FileExistsError("\n\033[0;31;40mCreate fastapi.yaml file failed!\033[0m\n")
+        
+        print("username:\t\t\t\033[1;36;40m", result[username]['username'],"\033[0m")
+        print("plain password:\t\t\t\033[1;36;40m", argv[2],"\033[0m")
+        print("hashed password:\t\t\033[1;36;40m", result[username]['hashed_password'],"\033[0m")
+        print("full name:\t\t\t\033[1;36;40m", result[username]['full_name'],"\033[0m")
+        print("email:\t\t\t\t\033[1;36;40m", result[username]['email'],"\033[0m")
+        print('account disabled:\t\t\033[1;36;40m', result[username]['disabled'],"\033[0m") 
+        
+    elif not os.path.exists("trained_model.h5") and not os.path.exists("fastapi.yaml"):
+        if(len(argv) != 10):
+            raise IOError("\n\033[0;33;40mPlease input info correctly! \n Format:\n1. username\n2. password\n3. full name\n4. email\n5. service_name\n6. region_name\n7. aws_access_key_id\n8. aws_secret_access_key\n9. aws_s3_bucket_name\033[0m\n")
+        else:
+            
+            username = argv[1]
+            result[username] = {}
+            result[username]['username'] = argv[1]
+            list_passwd = []
+            list_passwd.append(argv[2])
+            list_hashed_res = stauth.Hasher(list_passwd).generate()
+            result[username]['hashed_password'] = list_hashed_res[0]
+            result[username]['full_name'] = argv[3]
+            result[username]['email'] = argv[4]
+            result[username]['disabled'] = False
+            
+            
+            with open('fastapi.yaml', 'w', encoding='utf-8') as file:
+                yaml.dump(data=result, stream=file, allow_unicode=True)
+                
+            if os.path.exists("fastapi.yaml"):
+                print("\n\033[0;32;40mSuccessfully create `fastapi.yaml` file!\033[0m\n")
+            else:
+                raise FileExistsError("\n\033[0;31;40mCreate fastapi.yaml file failed!\033[0m\n")
+            
+            print("username:\t\t\t\033[1;36;40m", result[username]['username'],"\033[0m")
+            print("plain password:\t\t\t\033[1;36;40m", argv[2],"\033[0m")
+            print("hashed password:\t\t\033[1;36;40m", result[username]['hashed_password'],"\033[0m")
+            print("full name:\t\t\t\033[1;36;40m", result[username]['full_name'],"\033[0m")
+            print("email:\t\t\t\t\033[1;36;40m", result[username]['email'],"\033[0m")
+            print('account disabled:\t\t\033[1;36;40m', result[username]['disabled'],"\033[0m")
+    
+            
+            s3_credentials["service_name"] = argv[5]
+            s3_credentials["region_name"] = argv[6]
+            s3_credentials["aws_access_key_id"] = argv[7]
+            s3_credentials["aws_secret_access_key"] = argv[8]
+            s3_credentials["aws_s3_bucket_name"] = argv[9]
+
+            print("===========================================================================")        
+            print("service name:\t\t\t\033[1;36;40m",s3_credentials["service_name"],"\033[0m")
+            print("region name:\t\t\t\033[1;36;40m",s3_credentials["region_name"],"\033[0m")
+            print("aws access key id:\t\t\033[1;36;40m",s3_credentials["aws_access_key_id"],"\033[0m")
+            print("aws secret access key:\t\t\033[1;36;40m",s3_credentials["aws_secret_access_key"],"\033[0m")
+            print("aws s3 bucket name:\t\t\033[1;36;40m",s3_credentials["aws_s3_bucket_name"],"\033[0m\n")
+
+            s3_resource = boto3.client(
+                service_name=s3_credentials['service_name'],
+                region_name=s3_credentials['region_name'],
+                aws_access_key_id=s3_credentials['aws_access_key_id'],
+                aws_secret_access_key=s3_credentials['aws_secret_access_key'])
+
+            bucket = s3_credentials['aws_s3_bucket_name']
+
+            key = 'model/trained_model.h5'
+
+            s3_resource.download_file(bucket,key,Filename="trained_model.h5")
+            if os.path.exists("trained_model.h5"):
+                print("\n\033[0;32;40mSuccessfully create `trained_model.h5` file!\033[0m\n")
+             
+    else:
+        raise Exception("\n\033[0;31;40mSomething went wrong when we are trying to create config file for you! Please contact us for help!\033[0m\n")
+    
+except Exception as err:
+    print("\n\033[0;31;40mException: ",err,"\033[0m\n")
+    
+    if os.path.exists("key.txt"):
+        os.remove("key.txt")
+        print("\n\033[0;34;40m=== Remove key.txt file ===\033[0m\n")
+    if os.path.exists("fastapi.yaml"):
+        #os.remove("fastapi.yaml")
+        #print("\033[0;34;40m=== Remove fastapi.yaml file ===\033[0m\n")
+        print("\033[0;34;40m=== In production env, we wanna keep fastapi config until you delete the whole container or you need to delete it manually! ===\033[0m\n")
+        
+    if os.path.exists("trained_model.h5"):
+        #os.remove("trained_model.h5")
+        #print("\033[0;34;40m=== Remove trained_model.h5 file ===\033[0m\n")
+        print("\033[0;34;40m=== In order to reduce our bills in AWS S3. Once you download the trained model, we will keep it until you delete the whole container or you need to delete it manually! ===\033[0m\n")
+        
+    print("\033[0;33;40mSystem will exit in 3 seconds~\n\033[0m")
+    time.sleep(3)
+    sys.exit(1)
 else:
-    print("Your argvs are:\t", argv)
-    print("Length of argv is:\t", len(argv),"\n\n\n")
-    username = argv[1]
-    result[username] = {}
-    result[username]['username'] = argv[1]
-    list_passwd = []
-    list_passwd.append(argv[2])
-    list_hashed_res = stauth.Hasher(list_passwd).generate()
-    result[username]['hashed_password'] = list_hashed_res[0]
+    print("\033[0;32;40mHave Fun :D \033[0m\n\n")
+    print("You can copy your input info if you cannot remember...\n\nAfter 5 seconds, I will clear terminal and let you try our project!")
+    time.sleep(5)
+    os.system("clear")   
     
-    print("username:\t\t\t", argv[1])
-    print("plain password:\t\t\t", argv[2])
-    print("hashed password:\t\t", list_hashed_res[0])
-
-    if len(argv) == 3:
-        result[username]['full_name'] = argv[1]
-        result[username]['email'] = ""
-        print("full name:\t\t\t", argv[1])
-        print("email:\t\t\t\t", "")
-
-    elif(len(argv) == 4):
-        result[username]['full_name'] = argv[3]
-        result[username]['email'] = ""
-        print("full name:\t\t\t", argv[3])
-        print("email:\t\t\t\t", "")
-
-    elif(len(argv) == 5):
-        result[username]['full_name'] = argv[3]
-        result[username]['email'] = argv[4]
-        print("full name:\t\t\t", argv[3])
-        print("email:\t\t\t\t", argv[4])
-
-    result[username]['disabled'] = False
-    print('account disabled:\t\t', result[username]['disabled'])
-
-    with open('fastapi.yaml', 'w', encoding='utf-8') as file:
-        yaml.dump(data=result, stream=file, allow_unicode=True)
-        print("Successfully create `fastapi.yaml` file!")
-         
-    abs_path = os.path.dirname((os.path.abspath(__file__)))
-    yaml_path = abs_path + "/fastapi.yaml"
-    
-    with open(yaml_path, 'r') as file:
-        config = yaml.safe_load(file)
-        if username in config.keys():
-            user_dict = config[username]
-            print(user_dict)
-#########################################################
-
-
-
-
-
-
-
-
-
-
-
+######################################################## create `fastapi.yaml` and `trained_model.h5` ########################################################
 SECRET_KEY = key
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30 # 30 mins expire
